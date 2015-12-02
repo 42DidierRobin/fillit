068128f3c15d2380a7e4af7056c8da3cc231a409
Alexandre DESPONDS <adespond@student.42.fr>
2015-12-02 12:36:35 +0100

    add struct t_tris and t_conf

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: adespond <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 11:47:01 by adespond          #+#    #+#             */
-/*   Updated: 2015/12/02 11:48:25 by adespond         ###   ########.fr       */
+/*   Updated: 2015/12/02 12:36:08 by adespond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,20 @@
 
 # include <unistd.h>
 # include <stdio.h>
-# include
+# include <stdlib.h>
+
+typedef struct	s_tris
+{
+	char		name;
+	char		x;
+	char		y;
+}				t_tris;
+
+typedef struct	s_conf
+{
+	char		**grid;
+	s_tris		**piece;
+	char		nbr_piece;
+}				t_conf;
 
 #endif
