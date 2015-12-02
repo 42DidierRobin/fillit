4543d168de5b2c90a41e0d13207c5e56ca30c4fb
Alexandre DESPONDS <adespond@student.42.fr>
2015-12-02 18:08:08 +0100

    add plein de chose, ta gueule

---
+++ cread_tris.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cread_tris.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adespond <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/02 14:48:16 by adespond          #+#    #+#             */
+/*   Updated: 2015/12/02 17:38:23 by adespond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+t_tris	*init_tris(char name)
+{
+	t_tris	*tris;
+	t_point	*coord;
+	int		i;
+	
+	i = -1;
+	tris = (t_tris*)malloc(sizeof(t_tris));
+	tris->name = name;
+	while (++i < 4)
+	{
+		coord = (t_point*)malloc(sizeof(t_point));
+		coord->x = -1;
+		coord->y = -1;
+		tris->coord[i] = *coord;
+	}
+	return (tris);
+}
+
+char	create_tris(char *str, char name)
+{
+	t_tris	*tris;
+	int		i;
+	int		nbr;
+
+	i = 0;
+	nbr = -1;
+	tris = init_tris(name);
+	while (i < 20)
+	{
+		if (str[i] == '\n' && (i + 1) % 5 != 0)
+			return (0);
+		if (str[i] == '.' || str[i] == '#' || str[i] == '\n')
+		{
+			if (str[i] == '#')
+			{
+				if(++nbr < 4)
+					create_tris_coord(tris, nbr, i);
+				else
+					return (0);
+			}
+		}
+		else
+			return (0);
+		i++;
+	}
+	return (check_tris(tris));
+}
+
+void	create_tris_coord(t_tris *tris, int nbr, int i)
+{
+	t_point		point;
+
+	point.x = i % 5;
+	point.y = i / 5;
+	point.next = NULL;
+	tris->coord[nbr] = point;
+}
+
+char	check_tris(t_tris *tris)
+{
+	int		i;
+	t_point	*coord;
+
+	coord = tris->coord;
+	i = -1;
+	while (++i < 4)
+	{
+		if (coord[i].x < 0 || coord[i].x > 4 || coord[i].y < 0 || coord[i].y > 4)
+			return (0);
+	}
+	return (1);
+}
--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: adespond <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 11:47:01 by adespond          #+#    #+#             */
-/*   Updated: 2015/12/02 13:26:31 by rdidier          ###   ########.fr       */
+/*   Updated: 2015/12/02 17:33:18 by adespond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,34 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <fcntl.h>
+# include "libft/libft.h"
 
-typedef struct	s_point
+typedef struct		s_point
 {
-	char		x;
-	char		y;
-}				t_point;
+	char			x;
+	char			y;
+	struct s_point	*next;
+}					t_point;
 
-typedef struct	s_tris
+typedef struct		s_tris
 {
-	char		name;
-	t_point		coord[4];
-}				t_tris;
+	char			name;
+	t_point			coord[4];
+}					t_tris;
 
-typedef struct	s_conf
+typedef struct		s_conf
 {
-	char		**grid;
-	s_tris		**piece;
-	char		nbr_piece;
-}				t_conf;
+	char			**grid;
+	struct s_tris	list_tris[26];
+	char			nbr_piece;
+}					t_conf;
+
+char				open_file(char *file);
+char				read_file(int *fd);
+void				fillit(int *argc, char **argv);
+char				create_tris(char *str, char name);
+void				create_tris_coord(t_tris *tris, int nbr, int i);
+char				check_tris(t_tris *tris);
+t_tris				*init_tris(char name);
 #endif
