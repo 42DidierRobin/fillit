b79aa2fc9855b08ceb59f6b71ace532b05debbcd
Robin DIDIER <rdidier@e2r2p1.42.fr>
2015-12-18 10:02:02 +0100

    wegerah

--- algo.c
+++ algo.c
@@ -6,18 +6,13 @@
 /*   By: rdidier <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 12:29:16 by rdidier           #+#    #+#             */
-/*   Updated: 2015/12/09 15:07:06 by rdidier          ###   ########.fr       */
+/*   Updated: 2015/12/14 22:50:35 by rdidier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "fillit.h"
 
-//-----
-#include "stdio.h"
-//-----
-
-// Renvoi 1 si le point est un point de start (= un point adjacent (meme en diagonal) a une case remplie), 0 sinon
 int				ft_is_start(char **grid, short x, short y)
 {
 	short		i;
@@ -41,8 +36,7 @@ int				ft_is_start(char **grid, short x, short y)
 	return (0);
 }
 
-//Renvoi la liste des points de starts de la grille en cours
-t_point		*ft_give_starts(char **grid)
+t_point			*ft_give_starts(char **grid)
 {
 	t_point		*starts;
 	short		i;
@@ -70,13 +64,11 @@ t_point		*ft_give_starts(char **grid)
 	return (NULL);
 }
 
-// Verifie que la piece peut etre pose depuis start dans grid. Retourne 0 ou 1;
 short			ft_is_putable(char **grid, t_point *start, t_tris *piece)
 {
 	short	l;
 
 	l = 0;
-	//je verifi les 4 cases
 	while (4 - l)
 	{
 		if (((start->y + piece->coord[l].y >= 0)
@@ -89,8 +81,7 @@ short			ft_is_putable(char **grid, t_point *start, t_tris *piece)
 	return (1);
 }
 
-// Place une piece dans la grille a partir de start. Retourne grille;
-void		ft_put_piece(char **grid, t_point *start, t_tris *piece)
+void			ft_put_piece(char **grid, t_point *start, t_tris *piece)
 {
 	short	l;
 
@@ -103,23 +94,18 @@ void		ft_put_piece(char **grid, t_point *start, t_tris *piece)
 	}
 }
 
-//Pour linstant on prend les pieces dans lordre, on verra ensuite;
-void		ft_fill_this_shit(char **grid, t_conf *config, short iter, t_point *starts)
+void			ft_fill_this_shit(char **grid, t_conf *config, short iter, t_point *starts)
 {
-	//cas de sortie
 	if (iter == (config->nbr_piece))
 	{
-		//On a trouve plus petit on change le la grille de conf
 		if (ft_find_len(grid) <= config->min_size)
 		{
-			//liberer lancienne grille a faire !!!!
 			config->min_size = ft_find_len(grid);
 			config->grid = ft_copy_grid(grid, config->min_size);
 		}
 		ft_clean_grid(grid, config->list_tris[iter].name);
 		return ;
 	}
-	// si on a encore un point de demarage possible
 	if (starts->next)
 		ft_fill_this_shit(grid, config, iter, starts->next);
 	if (ft_is_putable(grid, starts, &config->list_tris[iter]))
@@ -131,18 +117,12 @@ void		ft_fill_this_shit(char **grid, t_conf *config, short iter, t_point *starts
 	ft_clean_grid(grid, config->list_tris[iter].name);
 }
 
-// Retourne la taille de plus petit carre constructible grace au pieces de t_conf
-void		ft_fillit(t_conf *config)
+void			ft_fillit(t_conf *config)
 {
 	char	**grid;
 	t_point	*begin;
 
 	begin = ft_new_element(0,0);
-	//if (config-->min_size == -1)
-	//		config->min_size = config->nbr_piece * 4;
-
-	// On creer la grille max, on pourra chercher a la diminuer plus tard
 	grid = ft_new_grid(config->nbr_piece * 4);
 	ft_fill_this_shit(grid, config, 0, begin);
 }
-
